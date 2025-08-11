5aff1c1a10697d04bda96b6b243773cc78a9d9d5
ayushghosh-123 <ghoshayush910@gmail.com>
2025-08-11 16:55:34 +0530

    changes in search movie

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,8 @@
 import { SignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const route = useRouter()
   return (
     <div className="flex justify-center items-center min-h-screen">
       <SignIn />
